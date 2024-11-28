f6736a7661572391cc9cd49450836915dbe3a7fd
fuongz <hi@phuongphung.com>
2024-11-28 14:42:06 +0700

    ✨ feat(landing): add test preview

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,18 +1,51 @@
 import "./App.css";
-import { VscGithubInverted } from "react-icons/vsc";
-import { useState } from "react";
+import { VscCopy, VscGithubInverted, VscPlay } from "react-icons/vsc";
+import { useEffect, useMemo, useState } from "react";
+import { useCopyToClipboard } from "react-use";
+
+const PROXY_URL: string = "https://img.phake.app/";
 
 function App() {
+  const [copied, setCopied] = useState<boolean>(true);
+  const [testUrl, setTestUrl] = useState<null | string>(null);
+  const [imageState, setImageState] = useState<string>("idle");
+  const [state, copyToClipboard] = useCopyToClipboard();
   const [format, setFormat] = useState<string>("png");
   const [imageUrl, setImageUrl] = useState<string>(
     "https://images.pexels.com/photos/27200179/pexels-photo-27200179/free-photo-of-landscape-of-hill-behind-flowers.jpeg",
   );
   const [resizeWidth, setResizeWidth] = useState<number | string>(200);
   const [resizeHeight, setResizeHeight] = useState<number | string>(200);
-  const [quality, setQuality] = useState<string | number>();
+  const [quality, setQuality] = useState<string | number>(70);
 
   const [error, setError] = useState<string | null>(null);
-  const [result, setResult] = useState<string>("");
+  const [options, setOptions] = useState<{ [key: string]: string | number }>({
+    format,
+    size: `${resizeWidth}, ${resizeHeight}`,
+    quality,
+  });
+
+  useEffect(() => {
+    if (!state.error && state.value) {
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 1000);
+    } else {
+      setCopied(false);
+    }
+  }, [state]);
+
+  const result = useMemo(() => {
+    return `${PROXY_URL}${
+      Object.keys(options).length > 0
+        ? Object.keys(options)
+            .filter((key) => options[key])
+            .map((key) => `${key}(${options[key]})`)
+            .join(":")
+        : "default"
+    }/${imageUrl}`;
+  }, [options]);
 
   const handleSubmit = () => {
     if (!imageUrl) {
@@ -22,22 +55,28 @@ function App() {
       setError(null);
     }
 
-    const options = [];
-    if (format) {
-      options.unshift(`format(${format})`);
-    }
+    const newOptions = {
+      format: options.format,
+      quality: options.quality,
+      size: options.size,
+    };
 
-    if (resizeWidth && resizeHeight) {
-      options.unshift(`size(${resizeWidth},${resizeHeight})`);
-    }
+    newOptions.format = format;
+    newOptions.size = `${resizeWidth}, ${resizeHeight}`;
+    newOptions.quality = quality;
 
-    if (quality) {
-      options.unshift(`quality(${quality})`);
-    }
+    setOptions(newOptions);
+  };
 
-    setResult(
-      `https://img.phake.app/${options.length > 0 ? options.join(":") : "default"}/${imageUrl}`,
-    );
+  const handleShowTestImage = () => {
+    if (result !== testUrl) {
+      setTestUrl(result);
+      setImageState("loading");
+    }
+  };
+
+  const handleOnLoadImage = () => {
+    setImageState("loaded");
   };
 
   return (
@@ -129,21 +168,64 @@ function App() {
             onClick={() => {
               handleSubmit();
             }}
-            className="bg-blue-600 font-semibold w-full block text-white shrink-0 rounded px-4 py-2"
+            className="bg-blue-600 font-semibold w-full block text-white shrink-0 rounded px-4 py-2 hover:bg-blue-500  transition  hover:transition"
           >
             Get
           </button>
         </div>
 
-        {!!result && (
+        {!!options && (
           <>
-            <div className="font-semibold mt-4">RESULT:</div>
-            <textarea
-              rows={5}
-              className="mt-2 w-full px-4 py-2 rounded bg-blue-50 border border-blue-200 text-blue-500"
-            >
-              {result}
-            </textarea>
+            <div className="flex justify-between items-center mt-4">
+              <div className="font-semibold mt-4">RESULT:</div>
+              <div
+                onClick={() => copyToClipboard(result)}
+                className=" text-sm flex gap-1 hover:bg-blue-500  transition  hover:transition text-white bg-blue-600 items-center top-2 py-1 right-4 rounded cursor-pointer px-2"
+              >
+                <VscCopy /> {copied ? "Copied!" : "Copy"}
+              </div>
+            </div>
+            <div className="mt-2 w-full px-4 pt-2 pb-2 text-wrap rounded bg-blue-50 border border-blue-200 text-zinc-400 relative">
+              {PROXY_URL}
+              {Object.keys(options).length > 0
+                ? Object.keys(options)
+                    .filter((key) => options[key])
+                    .map((key, index: number) => (
+                      <span key={`option-${key}`}>
+                        {index === 0 ? "" : ":"}
+                        <span className="text-orange-600">
+                          {key}(
+                          <span className="font-semibold">{options[key]}</span>)
+                        </span>
+                      </span>
+                    ))
+                : "default"}
+              /<span className="text-blue-600">{imageUrl}</span>
+            </div>
+
+            <div className="relative mt-4">
+              <div>
+                <button
+                  onClick={() => handleShowTestImage()}
+                  className="border-blue-600 text-sm inline-flex gap-4 items-center font-medium text-blue-600 border shrink-0 rounded px-4 py-2 hover:bg-blue-600 hover:text-white  transition  hover:transition"
+                >
+                  <VscPlay /> Preview
+                </button>
+              </div>
+
+              {!!testUrl && (
+                <div>
+                  {imageState === "loading" ? "Loading..." : <></>}
+
+                  <img
+                    onLoad={handleOnLoadImage}
+                    className={`${imageState !== "loaded" ? "hidden" : ""}`}
+                    src={testUrl}
+                    alt=""
+                  />
+                </div>
+              )}
+            </div>
           </>
         )}
 
